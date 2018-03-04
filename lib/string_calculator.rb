abf28c15a2bb20ffa5919cf115dd5137d48b8746
hvdson <hvdsonmusic@gmail.com>
2018-03-04 06:08:41 +0000

    finished tutorial

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,15 @@
 class StringCalculator
   def self.add(input)
-    0
+    if input == ""
+      0
+    elsif input.length == 1
+      input.to_i
+    else
+      # nums = input.split(',')
+      # nums[0].to_i + nums[1].to_i
+      # take each number from the split array and use map method to turn them to integers
+      nums = input.split(',').map { |num| num.to_i }
+      nums.reduce(:+)
+    end
   end
 end
